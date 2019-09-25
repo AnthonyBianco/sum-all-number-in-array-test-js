15117ecf4cb8cc8498e99d1c41771a7d88b90d46
AnthonyBianco <anthonysilvanobianco@gmail.com>
2019-09-25 15:55:05 +0000

    help from Fabio

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
-}
+ return array.reduce((total, elementOfArray) => {
+  if(Array.isArray(elementOfArray)){
+    return total + sumItems(elementOfArray); 
+  }
+  else {
+    return total + elementOfArray;
+  }
+}, 0);
+};
+
+console.log(sumItems([1,2,3,[3,3],3, 4]));
 
 module.exports = sumItems;
